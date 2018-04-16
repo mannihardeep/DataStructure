eefb5cd4acdd77f111caebf9fefe041d8ec13374
harde <harde@Hardeep-PC>
2018-04-16 20:54:08 +0530

    array rotate

---
+++ src/com/ds/array/ArrayRotationLeft.java
@@ -0,0 +1,34 @@
+package com.ds.array;
+
+public class ArrayRotationLeft {
+	int a[]= {1,2,3,4,5,6};
+	void print() {
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+	
+	void rotateLeft(int numberOfTimes) {
+		for(int i=0;i<numberOfTimes;i++) {
+			rotate();
+		}
+	}
+	
+	void rotate() {
+		int i;
+		int temp=a[0];
+		for(i=0;i<a.length-1;i++) {
+			a[i]=a[i+1];
+		}
+		a[i]=temp;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayRotationLeft obj= new ArrayRotationLeft();
+		obj.print();
+		obj.rotateLeft(2);
+		System.out.println();
+		obj.print();
+	}
+
+}
